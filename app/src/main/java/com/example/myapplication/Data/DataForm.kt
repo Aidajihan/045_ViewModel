392d4a0367eca86835af85d835529fbfad16fc25
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2023-10-25 00:36:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Data/DataForm.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication.Data
+
+data class DataForm(
+    val sex: String = ""
+)
---
+++ app/src/main/java/com/example/myapplication/Data/DataSource.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.Data
+
+import com.example.myapplication.R
+
+object DataSource {
+    val jenis = listOf(
+       R.string.laki,
+        R.string.perempuan
+    )
+}
